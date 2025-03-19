86cb4ab75c4a7dd729e47567021a9aa86a04c8e5
bussu <bussu@link.it>
2025-03-19 19:35:33 +0100

    aggiunti copyright

--- bo/core/dto/src/main/java/org/govway/catalogo/core/configurazione/DummyConfigurazioneExecutor.java
+++ bo/core/dto/src/main/java/org/govway/catalogo/core/configurazione/DummyConfigurazioneExecutor.java
@@ -1,3 +1,22 @@
+/*
+ * GovCat - GovWay API Catalogue
+ * https://github.com/link-it/govcat
+ *
+ * Copyright (c) 2021-2025 Link.it srl (https://link.it).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.govway.catalogo.core.configurazione;
 
 import java.util.HashMap;
@@ -29,10 +48,10 @@ public class DummyConfigurazioneExecutor implements IConfigurazioneExecutor {
 
 			stampaCampi(a);
 		} catch (JsonProcessingException e) {
-			logger.info("errore :"+e);
+			logger.info("errore :");
 
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
 		esito.setEsito(ESITO.OK);
 		esito.setMessaggioErrore("TUTTO OK");
