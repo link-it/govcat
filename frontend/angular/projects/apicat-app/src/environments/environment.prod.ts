186ddc947d31a8dfd402d042e31e41c777e9ccba
Alfonso Tatarelli <a.tatarelli@gmail.com>
2025-05-09 11:48:04 +0200

    FIx conflitti

--- frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
+++ frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
@@ -2,19 +2,9 @@ import { LogLevel } from "../models/log.interface";
 
 export const environment = {
   production: true,
-<<<<<<< HEAD
   version: '2.1.0',
-<<<<<<< HEAD
-  build: '250423.1540',
-=======
-  version: '2.0.5',
-  build: '250415.1146',
-
->>>>>>> origin/426-visibilita-amministrazione-configurabile-ruolo
-=======
   build: '250428.1715',
->>>>>>> 535c1530b546991b759ec90b12cc394c36a726b5
+
   logLevel: LogLevel.info,
   configFile: './assets/config/app-config.json'
 };
-
