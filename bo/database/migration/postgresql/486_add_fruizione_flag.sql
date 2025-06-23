68460c02314eaf070f5543d8eb96c0bc29c7328d
Giovanni De Luca <giovanni.deluca@link.it>
2025-06-23 17:56:06 +0200

    Issue 486: Fix DB patch

--- bo/database/migration/postgresql/486_add_fruizione_flag.sql
+++ bo/database/migration/postgresql/486_add_fruizione_flag.sql
@@ -1,2 +1,3 @@
-ALTER TABLE servizi
-ADD COLUMN fruizione BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE servizi ADD COLUMN fruizione BOOLEAN;
+update servizi set fruizione = (select esterna from organizations where id = (select id_organizzazione from soggetti where id = (select id_soggetto_referente from domini where id = (select id_dominio from servizi s where s.id=servizi.id))));
+ALTER TABLE servizi ALTER COLUMN fruizione SET NOT NULL;
