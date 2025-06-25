bdfabc7087bc8425631b6ff528eee3d89d428865
lamantia <diego.lamantia@link.it>
2025-06-25 17:49:33 +0200

    testsuite modulo business

--- frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
+++ frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { LogLevel } from "../models/log.interface";
 
 export const environment = {
   production: true,
-  version: '2.1.0',
-  build: '250528.2331',
+  version: '2.2.0',
+  build: '250625.1253',
 
   logLevel: LogLevel.info,
   configFile: './assets/config/app-config.json'
