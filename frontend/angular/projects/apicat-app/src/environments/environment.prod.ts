f8d00bd2de89b85f12735014af674e7bd7bd7121
Giovanni De Luca <giovanni.deluca@link.it>
2025-10-23 13:07:57 +0200

    Fix OWASP plugin issues

--- frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
+++ frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { LogLevel } from "../models/log.interface";
 
 export const environment = {
   production: true,
-  version: '2.2.0',
-  build: '251010.0821',
+  version: '2.3.0',
+  build: '251023.1236',
 
   logLevel: LogLevel.info,
   configFile: './assets/config/app-config.json'
