b3144175685d112338acbc31fe6fbcc545a05ffd
bussu <bussu@link.it>
2025-12-17 11:46:33 +0100

    eliminato issuer di default vuoto

--- frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
+++ frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { LogLevel } from "../models/log.interface";
 
 export const environment = {
   production: true,
-  version: '2.3.0',
-  build: '251023.1925',
+  version: '2.3.1',
+  build: '251216.1320',
 
   logLevel: LogLevel.info,
   configFile: './assets/config/app-config.json'
