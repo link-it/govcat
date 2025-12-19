6d481ed7d3f0767ff40072cb5251ace885734844
Giovanni De Luca <giovanni.deluca@link.it>
2025-12-19 10:54:24 +0100

    Version 2.2.5

--- frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
+++ frontend/angular/projects/apicat-app/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { LogLevel } from "../models/log.interface";
 
 export const environment = {
   production: true,
-  version: '2.2.0',
-  build: '250716.1121',
+  version: '2.2.5',
+  build: '251219.1026',
 
   logLevel: LogLevel.info,
   configFile: './assets/config/app-config.json'
