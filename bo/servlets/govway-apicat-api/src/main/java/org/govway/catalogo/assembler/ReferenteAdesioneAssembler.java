5bf3a38b26691336f7c772ad3a86c4cf34661652
Giovanni De Luca <giovanni.deluca@link.it>
2026-02-18 11:26:34 +0100

    Issue 168: bug fix

--- bo/servlets/govway-apicat-api/src/main/java/org/govway/catalogo/assembler/ReferenteAdesioneAssembler.java
+++ bo/servlets/govway-apicat-api/src/main/java/org/govway/catalogo/assembler/ReferenteAdesioneAssembler.java
@@ -122,7 +122,7 @@ public class ReferenteAdesioneAssembler extends RepresentationModelAssemblerSupp
 		UtenteEntity utente = utenteService.find(src.getIdUtente())
 				.orElseThrow(() -> new NotFoundException(ErrorCode.UT_404));
 		
-		if(!utente.getStato().equals(Stato.ABILITATO)) {
+		if(!utente.getStato().equals(Stato.ABILITATO) && !utente.getStato().equals(Stato.PENDING_UPDATE)) {
 			throw new BadRequestException(ErrorCode.UT_409, java.util.Map.of("nomeUtente", utente.getNome(), "cognomeUtente", utente.getCognome()));
 		}
 
