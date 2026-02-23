6013b43daafc9b1248df8ce52e6b6d5879ed4013
Giovanni De Luca <giovanni.deluca@link.it>
2026-02-23 17:59:05 +0100

    Revert "Improve Adesioni export performances"
    This reverts commit 5f6b8ecb6205d159e2aa6d98a9d19c482b14bd04.

--- bo/servlets/govway-apicat-api/src/main/java/org/govway/catalogo/controllers/csv/AdesioneCsvFormat.java
+++ bo/servlets/govway-apicat-api/src/main/java/org/govway/catalogo/controllers/csv/AdesioneCsvFormat.java
@@ -29,6 +29,10 @@ public abstract class AdesioneCsvFormat {
 	abstract String getErogatore();
 	@JsonProperty("Servizio")
 	abstract String getServizio();
+	@JsonProperty("API")
+	abstract String getApi();
+	@JsonProperty("Tipologia API")
+	abstract String getTipoApi();
 	@JsonProperty("Modalità Autenticazione")
 	abstract String getAutenticazioneStato();
 	@JsonProperty("Soggetto Aderente")
@@ -43,7 +47,19 @@ public abstract class AdesioneCsvFormat {
 	abstract String getReferenteTecnicoAdesione();
 	@JsonProperty("Applicativi Autorizzati (Prod)")
 	abstract String getApplicativiAutorizzatiProduzione();
+	@JsonProperty("Config. Autenticazione (Prod)")
+	abstract String getAutenticazioneValoreProduzione();
+	@JsonProperty("Rate Limiting (Prod)")
+	abstract String getRateLimitingProduzione();
+	@JsonProperty("URL Invocazione (Prod)")
+	abstract String getUrlInvocazioneProduzione();
 	@JsonProperty("Applicativi Autorizzati (Coll)")
 	abstract String getApplicativiAutorizzatiCollaudo();
+	@JsonProperty("Config. Autenticazione (Coll)")
+	abstract String getAutenticazioneValoreCollaudo();
+	@JsonProperty("Rate Limiting (Coll)")
+	abstract String getRateLimitingCollaudo();
+	@JsonProperty("URL Invocazione (Coll)")
+	abstract String getUrlInvocazioneCollaudo();
 
 }
