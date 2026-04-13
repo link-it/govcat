592d54c29fc1dd05adda67009f2558aa0534f984
Giovanni De Luca <giovanni.deluca@link.it>
2026-04-13 11:07:11 +0200

    Issue 243: add csp-nonce meta to fix csp vulnerability for web/tomcat FE deployment

--- web/src/main/java/org/govway/catalogo/reverse_proxy/security/SecurityConfig.java
+++ web/src/main/java/org/govway/catalogo/reverse_proxy/security/SecurityConfig.java
@@ -90,8 +90,7 @@ public class SecurityConfig {
    			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
    		.and()
    			.headers()
-   			.xssProtection().and()
-				.contentSecurityPolicy("default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; form-action 'self'; base-uri 'self'")
+   			.xssProtection()
 		;
    		
    		return http.build();
